80e509e4357a60d0294c0c7b15320bbc069ac3a1
Aksh Gupta <akshgpt7@gmail.com>
2021-12-06 01:08:32 +0530

    fix getting selection

--- src/main_menu.py
+++ src/main_menu.py
@@ -1,26 +1,27 @@
-from prompt_toolkit.widgets import MenuContainer, MenuItem
+from prompt_toolkit.widgets import MenuContainer
 from prompt_toolkit.layout.containers import Window
 
 from keybindings import kb
-from dir_data import cwd_data
+from dir_data import data, menu_data
+from utils import get_menu_selection_path
 
 menu_container = Window(style="fg:ansiwhite bg:ansiblack")
-# menu = MenuContainer(body=menu_container, menu_items=[MenuItem(text="hello", children=[MenuItem(text="1", children=[MenuItem(text="haha"), MenuItem(text="hehe")]),
-                                                                                       # MenuItem(text="2")]), MenuItem(text="world")])
 
-menu = MenuContainer(body=menu_container, menu_items=cwd_data)
+menu = MenuContainer(body=menu_container, menu_items=menu_data)
 
 
 
 
 
 
-
+@kb.add('o')
+def start_file(event):
+    print("hehe")
 
 
 
 
 @kb.add('r')
 def rename(event):
-    print(menu.selected_menu)
+    get_menu_selection_path(data, menu.selected_menu)
 
