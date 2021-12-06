8a8043ee4c45064449bdf3731009018bb70e6b8b
Aksh Gupta <akshgpt7@gmail.com>
2021-12-06 21:41:38 +0530

    restructure and add styled heading

--- src/termfm.py
+++ src/termfm.py
@@ -1,35 +1,74 @@
 from prompt_toolkit import Application
-from prompt_toolkit.layout import Dimension, WindowAlign
-from prompt_toolkit.widgets import HorizontalLine, VerticalLine, MenuContainer, MenuItem
+from prompt_toolkit.layout import WindowAlign
+from prompt_toolkit.widgets import MenuContainer, TextArea
 from prompt_toolkit.layout.containers import Window, HSplit, VSplit
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.widgets import MenuContainer
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.application import get_app
 
-from keybindings import kb
-from main_menu import menu
+import subprocess, sys
+from art import text2art
+
+from utils import get_input, get_menu_selection
+from dir_data import data, menu_data
 
 
-############## show home folder contents by default, have a window at left for favs and common folders
+kb = KeyBindings()
+menu_kb = KeyBindings()
 
+heading = text2art("TERMFM", font="cyberlarge")
 
+menu_container = Window(style="fg:ansiwhite bg:ansiblack")
+menu = MenuContainer(body=menu_container, menu_items=menu_data)
+menu_env = VSplit([menu], key_bindings=menu_kb)
 
-menu_env = VSplit([
-    Window(width=Dimension(max=5)),  # makes a left side container (use it for favs later)
-    VerticalLine(),
-    menu,
-    ])
-
+prompt_field = Window(dont_extend_height=True, content=FormattedTextControl(focusable=False), style="bg:ansigray fg:ansiblack")
+user_input_field = TextArea(multiline=False, focusable=True, dont_extend_height=True, style="bg:ansigray fg:ansiblack")
 root_container =  HSplit([
-    Window(height=Dimension(max=1), align=WindowAlign.CENTER, style="fg:ansiwhite bg:ansiblack", content=FormattedTextControl(text="TERMFM", focusable=False)),
-    HorizontalLine(),
+    Window(dont_extend_height=True, align=WindowAlign.CENTER, style="fg:ansiwhite bg:ansiblack", content=FormattedTextControl(text=heading, focusable=False)),
     menu_env,
+    prompt_field,
+    user_input_field,
 ])
 
 layout = Layout(root_container, focused_element=menu)
 
 
+# -------------------------------  KEYBINDINGS  --------------------------
+
+
+# Exit
+@kb.add('c-q')
+def exit_(event):
+    event.app.exit()
+
+
+# KEYBINDINGS FOR MENU
+
+@menu_kb.add('o')
+# open a file with its associated program
+def start_file(event):
+    selection = get_menu_selection(data, menu.selected_menu)
+    if selection['type'] == "file":
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, selection['path']])
+    else:
+        pass
+
+
+@menu_kb.add('d')
+# delete a file
+def delete_file(event):
+    get_app().layout.focus(user_input_field)
+
+
+
+@menu_kb.add('r')
+def rename(event):
+    get_menu_selection(data, menu.selected_menu)
+
+
 app = Application(key_bindings=kb, layout=layout, full_screen=True)
 app.run()
-
-
-
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
+import os
 
-def get_menu_selection_path(data, selection):
+
+def print_dir(dir_name):
+    return("📁 " + dir_name)
+
+
+def print_file(file_name):
+    return("📄 " + file_name)
+
+
+def get_input(buffer):
+    return 
+
+
+def get_menu_selection(data, selection):
     folder_index = 0  # which tuple we are currently at
     contents_index = 0  # to search in folders list or files list of that tuple
     final_index = 0  # position of selected file/folder in its list
@@ -13,29 +27,36 @@ def get_menu_selection_path(data, selection):
     folder_index -= 1
 
     if selection[-1] >= len(data[folder_index][1]):
-        # file
+        # search inside files list
         contents_index = 2
         final_index = selection[-1] - len(data[folder_index][1])
     else:
         contents_index = 1
         final_index = selection[-1]
 
+    selection_name = data[folder_index][contents_index][final_index]
+    selection_path = os.path.join(data[folder_index][0], selection_name)
+    
+    return_dict = {'path': selection_path, 'name': selection_name, 'type': ""}
 
+    if os.path.isfile(selection_path):
+        return_dict['type'] = "file"
+    else:
+        return_dict['type'] = "folder"
 
+    return return_dict
 
     # print(folder_index, contents_index, final_index)
-    print(data[folder_index][contents_index][final_index])
-        # print(selection)
+    # print(data[folder_index][contents_index][final_index])
+    # print(selection)
 
 
 def find_skips(data, index):
     if len(data[index][1]) == 0:
-        # print(index)
         return [1, 0]
 
     output = [1, 0]
     sum = 0
-
 
     for i in range(len(data[index][1])):
         if index == 7:
@@ -47,14 +68,4 @@ def find_skips(data, index):
     output[1] = len(data[index][1])
 
     return output
-
-
-
-
-def print_dir(dir_name):
-    return("📁 " + dir_name)
-
-def print_file(file_name):
-    return("📄 " + file_name)
-
 
